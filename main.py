cf0c717efeba1a9ba7dba379414440290567afa3
eugen <b-eugen@yandex.ru>
2018-10-03 22:59:42 +0300

    PredLSTM

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 
 def main():
     Sig = SignalsHeader.AsiSignal()
-    path = '2018_07_06 11_05_39.sig'
+    path = '2017_08_22 22_11_12.sig'
     result = Sig.getdata(path)
+    result['avg'] = result.mean(axis=1)
+    result['diff'] = result['p1_ch2'].diff()
     ax = plt.gca()
-    result.plot(x='datetime', y='p1_ch1', ax=ax)
-    result.plot(x='datetime', y='p1_ch2', ax=ax)
-    result.plot(x='datetime', y='p1_ch3', ax=ax)
-    result.plot(x='datetime', y='p1_ch4', ax=ax)
+    #result.plot(x='datetime', y='avg', ax=ax)
+    result.plot(x='datetime', y='diff', ax=ax)
+    #result.plot(x='datetime', y='p1_ch3', ax=ax)
+    #result.plot(x='datetime', y='p1_ch4', ax=ax)
     plt.show()
     return result
 
