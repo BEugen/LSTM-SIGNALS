11d8cb2ecb53095470163f719edc8a25672986a7
BattsEA <b-eugen@yandex.ru>
2018-10-03 18:02:46 +0300

    LSTM

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import  SignalsHeader
+import SignalsHeader
+import matplotlib.pyplot as plt
 
 
 def main():
     Sig = SignalsHeader.AsiSignal()
-    path = 'test.sig'
+    path = '2018_07_06 11_05_39.sig'
     result = Sig.getdata(path)
+    ax = plt.gca()
+    result.plot(x='datetime', y='p1_ch1', ax=ax)
+    result.plot(x='datetime', y='p1_ch2', ax=ax)
+    result.plot(x='datetime', y='p1_ch3', ax=ax)
+    result.plot(x='datetime', y='p1_ch4', ax=ax)
+    plt.show()
     return result
 
 
