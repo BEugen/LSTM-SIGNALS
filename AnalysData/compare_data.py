b4257b6bef8359984fb6f3cfe5925131575345d4
eugen <b-eugen@yandex.ru>
2018-10-13 00:21:57 +0300

    LSTM

--- AnalysData/compare_data.py
+++ AnalysData/compare_data.py
@@ -4,31 +4,33 @@ from datetime import timedelta, datetime
 
 
 def main():
-    pd_bed = pd.read_csv('E:/TMP/rs/bax/2018_08_25 04_42_50.csv', sep=';', parse_dates=[1])
-    pd_good = pd.read_csv('E:/TMP/rs/gd/2018_08_30 12_32_49.csv', sep=';', parse_dates=[1])
+    pd_bed = pd.read_csv('/mnt/data/data/LSTM-RL/rs/ep/bax/2018_08_24 14_22_31.csv', sep=';', parse_dates=[1])
+    pd_good = pd.read_csv('/mnt/data/data/LSTM-RL/rs/ep/gd/2018_09_22 07_38_43.csv', sep=';', parse_dates=[1])
     stdtimr = pd_bed.iloc[0, 1]
     pd_bed['datetime'] = pd_bed['datetime'] - stdtimr
     stdtimr = pd_good.iloc[0, 1]
     pd_good['datetime'] = pd_good['datetime'] - stdtimr
     pd_bed['diffb'] = pd_bed['p1_ch3'].diff()
     pd_good['diffb'] = pd_good['p1_ch3'].diff()
-    pd_avg = wincurve(pd_bed)
-    pdg_avg = wincurve(pd_good)
-    fig, (ax1, ax3) = plt.subplots(nrows=2, ncols=1)
+    pd_avg_b = wincurve(pd_bed)
+    print(pd_avg_b.shape)
+    pd_avg_g = wincurve(pd_good)
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(20, 10))
    # pd_bed.plot(x='datetime', y='p1_ch4', ax=ax1)
    # pd_good.plot(x='datetime', y='p1_ch4', ax=ax1)
-    plt.gcf().autofmt_xdate()
-    pd_bed.plot(x='datetime', y='diffb', ax=ax1)
-    pd_good.plot(x='datetime', y='diffb', ax=ax1)
-    pdg_avg.plot(x='datetime', y='diffb', ax=ax3)
-    pd_avg.plot(x='datetime', y='diffb', ax=ax3)
+    pd_bed.plot(x='datetime', y='p1_ch3', ax=ax2, color='red', label='bad ch')
+    pd_good.plot(x='datetime', y='p1_ch3', ax=ax2, color='green', label='good ch')
+    pd_bed.plot(x='datetime', y='diffb', ax=ax1, color='red', label='bad dy/dx')
+    pd_good.plot(x='datetime', y='diffb', ax=ax1, color='green', label='good dy/dx')
+    pd_avg_b.plot(x='datetime', y='diffb', ax=ax3, color='red', label='bad ac dy/dx')
+    pd_avg_g.plot(x='datetime', y='diffb', ax=ax3, color='green', label='good ac dy/dx')
     plt.show()
 
 
 def wincurve(data):
     #timedelta(seconds=addtime)
     offset = 2.0
-    winsize = 1.5
+    winsize = 1.0
     stdtimr = data.iloc[0, 1] + timedelta(seconds=winsize)
     endtimr = data.iloc[data.shape[0] - 1, 1]
     rows = []
