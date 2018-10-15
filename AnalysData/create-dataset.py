6ebff2a445e6692624edfcc8cc1ead9ae48f7b94
BattsEA <b-eugen@yandex.ru>
2018-10-15 12:17:40 +0300

    LSTM

--- AnalysData/create-dataset.py
+++ AnalysData/create-dataset.py
@@ -1,26 +1,36 @@
 import os
 from datetime import timedelta, datetime
 import pandas as pd
+import json
 
-
+column = ['p1_ch1', 'p1_ch2', 'p1_ch3', 'p1_ch4']
 ch_null = {'1': 142, '2': 104, '3': 123, '4': 146}
+PATH_JSON = 'E:/TMP/rs/jsn/'
+PATH_FILE = 'E:/TMP/rs/ep/gd/'
 
 def main():
-    pd_bed = pd.read_csv('/mnt/data/data/LSTM-RL/rs/ep/bax/2018_08_24 14_22_31.csv', sep=';', parse_dates=[1])
-    df = windsmotch(pd_bed, ['p1_ch1', 'p1_ch2', 'p1_ch3', 'p1_ch4'])
-    for i in range(1, 5):
-        df['p1_ch' + str(i)] = df['p1_ch' + str(i)] - ch_null[str(i)]
-    print(df)
+    dataset = pd.DataFrame([], columns=column)
+    for file in os.listdir(PATH_FILE):
+        pd_bed = pd.read_csv(PATH_FILE + file, sep=';', parse_dates=[1])
+        df = windsmotch(pd_bed, column)
+        ch_null = getnullforchannels(PATH_JSON + os.path.splitext(file)[0] + '.json')
+        for i in range(1, 5):
+            df['p1_ch' + str(i)] = df['p1_ch' + str(i)] - ch_null[str(i)]
+        ind = df[df[column] > 50.0].sort_index(ascending=0).first_valid_index()
+        df.drop('datetime', axis=1)
+        print(file, df[column].max())
+        dataset = dataset.append(df[0: ind+10].drop('datetime', axis=1))
+    dataset.to_csv('railsdataset.csv', sep=';', index=False)
 
-def windsmotch(data, column):
-    #timedelta(seconds=addtime)
+
+def windsmotch(data, col):
     offset = 0.5
     winsize = 0.15
     stdtimr = data.iloc[0, 1] + timedelta(seconds=winsize)
     endtimr = data.iloc[data.shape[0] - 1, 1]
     rows = []
     columns = ['datetime']
-    for x in column:
+    for x in col:
         columns.append(x)
     while stdtimr <= endtimr:
         row = [stdtimr]
@@ -29,7 +39,21 @@ def windsmotch(data, column):
             row.append(i)
         rows.append(row)
         stdtimr += timedelta(seconds=offset)
-    return pd.DataFrame(rows, columns=columns)
+    df = pd.DataFrame(rows, columns=columns)
+    return df
+
+
+def getnullforchannels(file):
+    f = open(file)
+    data = json.load(f)
+    f.close()
+    chnull = {}
+    p = data['platforms'][0]
+    chnull['1'] = p['Canal1NullCode']
+    chnull['2'] = p['Canal2NullCode']
+    chnull['3'] = p['Canal3NullCode']
+    chnull['4'] = p['Canal4NullCode']
+    return chnull
 
 
 if __name__ == '__main__':
